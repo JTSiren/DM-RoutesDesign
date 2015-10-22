7ea647cd1b657cdd920c571a08db38eb72149062
J Siren <jsiren90549@gmail.com>
2015-10-22 16:27:34 -0700

    contains most if not all of original design specs. still needs sign-in page

--- client/router.js
+++ client/router.js
@@ -9,9 +9,11 @@ Router.map(function(){
 	this.route('profile', {path: "/profile"});
 	this.route('locations', {path: "/locations"});
 	this.route('map', {path: "/map"});
-	this.route('transit-lines', {path: "/transit-lines"});
-	this.route('line-direction', {path: "/line-direction"});
+	this.route('transit_lines', {path: "/transit_lines"});
+	this.route('lineDirection', {path: "/lineDirection"});
 	this.route('stops', {path: "/stops"});
 	this.route('boots', {path: "/boots"});
+	this.route('edit_locations', {path: "/edit_locations"});
+
 
 });
